2dc67c21724e39db09242d668807444b48aa55da
训练有素的藤原白叶 <1751842477@qq.com>
2022-01-28 10:37:52 +0000

    加入Setblock和末点特效

--- Particle_Music/Particle_file_control.h
+++ Particle_Music/Particle_file_control.h
@@ -2,13 +2,15 @@
 #pragma warning(disable : 4996)
 #ifndef FILE_CONTROL_PARTICLE
 #define FILE_CONTROL_PARTICLE
-#include "D:\VisualStudioProj\MCœ‡πÿ\Particle_Music\Particle.h"
+#include "D:\VisualStudioProj\MCÁõ∏ÂÖ≥\Particle_Music\Particle.h"
 
-//Œƒº˛∏Ò Ω£∫√ø“ª––∑÷±Œ™£∫∆ º ±º‰	£®ÀÆ∆Ω÷∆±Ì∑˚£© “Ù∏ﬂ
+//Êñá‰ª∂Ê†ºÂºèÔºöÊØè‰∏ÄË°åÂàÜÂà´‰∏∫ÔºöËµ∑ÂßãÊó∂Èó¥	ÔºàÊ∞¥Âπ≥Âà∂Ë°®Á¨¶Ôºâ Èü≥È´ò
 
 
 FILE* PARTICLEFILECONTROL = nullptr;
+FILE* SBPARTICLEFILECONTROL = nullptr;
 char PARTICLEFILELOCATE[255] = "Input.txt";
+char SBPARTICLEFILELOCATE[255] = "Input_SB.txt";
 vector<vector<int>> MIDI_INFO_PARTICLE;
 
 void open_file() {
@@ -43,6 +45,33 @@ void print_txt_file() {
 	for (int i = 0; i < (int)MIDI_INFO_PARTICLE.size(); i++) {
 		for (int j = 0; j < (int)MIDI_INFO_PARTICLE[i].size(); j++) {
 			printf("%d    ", MIDI_INFO_PARTICLE[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void open_sound_block_file() {
+	SBPARTICLEFILECONTROL = fopen(SBPARTICLEFILELOCATE, "a+");
+}
+
+vector <vector<int>> SBNOTEINFO;
+
+void load_SB_Particle_file() {
+	srand(time(NULL));
+	int SBTEMP_NOTE[3];
+	for (int i = 0; fscanf(SBPARTICLEFILECONTROL, "%d %d %d", SBTEMP_NOTE, SBTEMP_NOTE + 1, SBTEMP_NOTE + 2) != EOF;) {
+		vector<int> TEMPSB;
+		TEMPSB.push_back(*SBTEMP_NOTE);
+		TEMPSB.push_back(*(SBTEMP_NOTE+1));
+		TEMPSB.push_back(*(SBTEMP_NOTE+2));
+		SBNOTEINFO.push_back(TEMPSB);
+	}
+}
+
+void print_SB_file() {
+	for (int i = 0; i < (int)SBNOTEINFO.size(); i++) {
+		for (int j = 0; j < 3; j++) {
+			printf("%d    ", SBNOTEINFO[i][j]);
 		}
 		printf("\n");
 	}
